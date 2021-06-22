5ca077c0b99ba63d85af7264e8667d8a587e3df3
gyu-don <takumi.kt+git@gmail.com>
2021-06-22 10:18:28 +0900

    API: Remove Aspen8 device because it is retired.

--- bqcloud/device.py
+++ bqcloud/device.py
@@ -4,7 +4,6 @@ from enum import Enum
 class Device(str, Enum):
     Local = "local"
     IonQDevice = "aws/ionq/ionQdevice"
-    Aspen8 = "aws/rigetti/Aspen-8"
     Aspen9 = "aws/rigetti/Aspen-9"
     SimSv1 = "aws/amazon/sv1"
     SimTn1 = "aws/amazon/tn1"
