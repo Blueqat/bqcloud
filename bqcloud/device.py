78226e0aae75abcdc82d4c12ed99cf85b55ad51d
Yuichiro Minato <minato@blueqat.com>
2023-03-16 15:44:36 +0900

    Update device.py

--- bqcloud/device.py
+++ bqcloud/device.py
@@ -4,7 +4,7 @@ from enum import Enum
 class Device(str, Enum):
     Local = "local"
     IonQDevice = "aws/ionq/ionQdevice"
-    AspenM2 = "aws/rigetti/Aspen-M-2"
+    AspenM3 = "aws/rigetti/Aspen-M-3"
     SimSv1 = "aws/amazon/sv1"
     SimTn1 = "aws/amazon/tn1"
     SimDm1 = "aws/amazon/dm1"
