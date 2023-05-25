e22cc4383762fb270552437d7fdb5efa866345bd
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2023-05-25 15:06:01 +0900

    add device IonQAria1, IonQHarmony and OqcLucy

--- bqcloud/device.py
+++ bqcloud/device.py
@@ -4,6 +4,9 @@ from enum import Enum
 class Device(str, Enum):
     Local = "local"
     IonQDevice = "aws/ionq/ionQdevice"
+    IonQAria1 = "aws/ionq/Aria-1"
+    IonQHarmony = "aws/ionq/Harmony"
+    OqcLucy = "aws/oqc/Lucy"
     AspenM3 = "aws/rigetti/Aspen-M-3"
     SimSv1 = "aws/amazon/sv1"
     SimTn1 = "aws/amazon/tn1"
